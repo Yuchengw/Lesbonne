b4e95c3cb4e9e96450d6038d3573b402460dca4e
Yuchengw <ycwmike@gmail.com>
2016-01-03 19:43:54 -0800

    add hashable password and refactor user update, delete, create

--- core/appserver/src/main/java/com/lesbonne/bean/rest/controller/UserRestURIConstants.java
+++ core/appserver/src/main/java/com/lesbonne/bean/rest/controller/UserRestURIConstants.java
@@ -5,8 +5,9 @@ package com.lesbonne.bean.rest.controller;
  * @since 1
  * */
 public class UserRestURIConstants {
-	 public static final String GET_USER = "/service/user/{userEmail}";
+	 public static final String GET_USER_BY_EMAIL = "/service/user/{userEmail}";
+	 public static final String GET_USER_BY_ID = "/service/user/userId}";
 	 public static final String CREATE_USER = "/service/user/signup";
 	 public static final String UPDATE_USER = "/service/user/update";
-	 public static final String DELETE_USER = "/service/user/delete/{user}";
+	 public static final String DELETE_USER = "/service/user/delete";
 }
--- core/platformserver/src/main/java/com/lesbonne/user/UserRestController.java
+++ core/platformserver/src/main/java/com/lesbonne/user/UserRestController.java
@@ -6,11 +6,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 public interface UserRestController {
 		
-	public ResponseEntity<User> getUser(@PathVariable String email);
+	public ResponseEntity<User> getUserByEmail(@PathVariable String email);
 	
 	public ResponseEntity<User> updateUser(@RequestBody final User user);
 	
-	public ResponseEntity<String> addUser(@RequestBody User user);
+	public ResponseEntity<User> addUser(@RequestBody User user);
 	
-	public ResponseEntity<String> deleteUser(@PathVariable User user);
+	public ResponseEntity<Boolean> deleteUser(@PathVariable User user);
 }

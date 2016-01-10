e14ceab2f7623c38f07538b19ab25b986435b240
Yuchengw <ycwmike@gmail.com>
2016-01-09 21:18:52 -0800

    setting up UI dev- step one

--- core/ui/src/main/webapp/resources/angular/app/components/notification/notification.js
+++
@@ -1,5 +0,0 @@
-angular.module("notificationApp", ['localStore'])
-.controller("notificationAppController", ['$scope','$location', '$http', 'TokenStorage', 
-    function($scope, $location, $http, TokenStorage){
-    $scope.message = "message from yummet:)";
-}]);

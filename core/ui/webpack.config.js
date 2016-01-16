b135212b10241c25f99f4e7a47987f1446bda048
Yuchengw <ycwmike@gmail.com>
2016-01-16 00:05:32 -0800

    add configure

--- core/ui/webpack.config.js
+++ core/ui/webpack.config.js
@@ -3,12 +3,15 @@ var webpack = require('webpack');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-	entry: {
-	    "app": path.resolve(__dirname, "./src/main/webapp/assets/js/app/app.js")
-	},
+	entry: 
+	    // "app": path.resolve(__dirname, "./src/main/webapp/assets/js/entry.js")
+	    getEntrySrouce(['./src/main/webapp/assets/js/entry.js']),
+
 	output: {
-    	path: path.resolve(__dirname, './src/main/webapp/assets/js/app/app.js'),
-    	filename: 'bundle.js',		
+    	// path: path.resolve(__dirname, './src/main/webapp/assets/js/entry.js'),
+    	// filename: 'bundle.js',
+    	publicPath: 'http://localhost:8000/',
+    	filename: 'prod/bundle.js'		
 	},
 	externals: {
 
@@ -20,12 +23,51 @@ module.exports = {
 	sassLoader: {
 
 	},
-	module: {
-		preLoaders: [
-		],
-		loaders: [
-		],
-	},
+    module: {
+        preLoaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'source-map'
+            }
+        ],
+        loaders: [
+            {
+                test: /\.scss$/,
+                include: /src\/main\/webapp\/assets/,
+                loaders: [
+                    'style',
+                    'css',
+                    'autoprefixer?browsers=last 3 versions',
+                    'sass?outputStyle=expanded'
+                ]
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg)$/i,
+                loaders: [
+                    'url?limit=8192',
+                    'img'
+                ]
+            },
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules|bower_components)/,
+                loaders: [
+                    'react-hot',
+                    'babel?stage=0'
+                ]
+            }
+        ]
+    },
 	plugins: [
 	]
 };
+
+function getEntrySources(sources) {
+    if (process.env.NODE_ENV !== 'production') {
+        sources.push('webpack-dev-server/client?http://localhost:8080');
+        sources.push('webpack/hot/only-dev-server');
+    }
+
+    return sources;
+}

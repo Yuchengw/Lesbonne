b317576b828c118fabf6c1ff0fcbfb41127ba83d
Jassica-jiafei <jassica.jiafei@gmail.com>
2016-01-17 21:10:36 -0800

    Save the image to the local file system and refactor the test framework

--- core/platformserver/src/main/java/com/lesbonne/images/RelatedImagesRemoteSaver.java
+++ core/platformserver/src/main/java/com/lesbonne/images/RelatedImagesRemoteSaver.java
@@ -1,20 +1,26 @@
 package com.lesbonne.images;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Calendar;
 
+import org.apache.commons.io.FileUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 /**
  * The helper method to help save the image to the remote server
+ * 
  * @author jassica
  *
  */
 public class RelatedImagesRemoteSaver {
-	private final String FILE_DIVIDER = "/";
+	private final String FILE_DIVIDER = System.getProperty("file.separator");
 
 	/**
-	 * The file location will be something like images/[year]/[month]/[date]/[hour]/[minute]/filename
-	 * The reason for the format should be found under spike document
+	 * The file location will be something like
+	 * images/[year]/[month]/[date]/[hour]/[minute]/filename The reason for the
+	 * format should be found under spike document
+	 * 
 	 * @param fileName
 	 * @return
 	 */
@@ -34,7 +40,21 @@ public class RelatedImagesRemoteSaver {
 		return imageRemoteLocation.toString();
 	}
 
+	/**
+	 * Before production, all the pictures will save locally
+	 * 
+	 * @param path
+	 * @param file
+	 */
 	public void saveToRemoteServer(String path, MultipartFile file) {
+		// String osname = System.getProperty("os.name", "").toLowerCase();
+		String homeDir = System.getProperty("user.home");
+		File outputFile = new File(homeDir, path);
+		try {
+			FileUtils.copyInputStreamToFile(file.getInputStream(), outputFile);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
 	}
 }
---
+++ core/platformserver/src/test/java/com/lesbonne/user/mocktest/UserGetTest.java
@@ -0,0 +1,90 @@
+package test.java.com.lesbonne.user.mocktest;
+
+import com.jayway.restassured.http.ContentType;
+import com.jayway.restassured.module.mockmvc.RestAssuredMockMvc;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Matchers;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.context.WebApplicationContext;
+
+import javax.servlet.http.HttpServletResponse;
+
+import com.lesbonne.user.User;
+import com.lesbonne.user.UserService;
+
+import static com.jayway.restassured.module.mockmvc.RestAssuredMockMvc.given;
+import static org.hamcrest.CoreMatchers.equalTo;
+
+/**
+ * @author Jassica
+ * @since 1
+ * */
+public class UserGetTest extends BaseRestUserControllerTest {
+
+	@Test
+	@Transactional
+	public void testExistsUserByEmailWithValidInput() {
+		// Fake the userService call on existsUserByEmail
+		// return true all the time if the input is TESTEMAIL
+		Mockito.when(userService.existsUserByEmail(TEST_EMAIL)).thenReturn(true);
+
+		given().when().get(URLPREFIX + "existsUserByEmail" + "/" + TEST_EMAIL)
+				.then().statusCode(HttpServletResponse.SC_OK)
+				.contentType("application/json").body(equalTo("true"));
+
+		// Testing user with non-existing email, return false
+		org.mockito.Mockito.when(userService.existsUserByEmail(TEST_EMAIL))
+				.thenReturn(false);
+
+		given().when().get(URLPREFIX + "existsUserByEmail" + "/" + TEST_EMAIL)
+				.then().statusCode(HttpServletResponse.SC_OK)
+				.contentType("application/json").body(equalTo("false"));
+	}
+
+	@Test
+	@Transactional
+	public void testNoExistsUserByEmailWithValidInput() {
+		// Fake the userService call on existsUserByEmail
+		// return false all the time if the input is TESTEMAIL
+		Mockito.when(userService.existsUserByEmail(TEST_EMAIL))
+				.thenReturn(false);
+
+		given().when().get(URLPREFIX + "existsUserByEmail" + "/" + TEST_EMAIL)
+				.then().statusCode(HttpServletResponse.SC_OK)
+				.contentType("application/json").body(equalTo("false"));
+	}
+	
+//	@Test
+//	public void testGetAExistUser() {
+//		// expect().body("userPassword", equalTo("1234"))
+//		// .when()
+//		// .get("http://localhost:8092/platform/v1/user/email/test1@test.com");
+//		given().contentType("application/json").when().get("/test1@test.com/")
+//				.then().statusCode(200).body("userPassword", equalTo("1234"));
+//		// expect().
+//		// statusCode(200).
+//		// body(
+//		// "success", equalTo(true),
+//		// "userInfo.userId", equalTo("wadexu"),
+//		// "userInfo.firstName", equalTo("Wade"),
+//		// "userInfo.lastName", equalTo("Xu"),
+//		// "error", equalTo(null)).
+//		// when().
+//		// get("/user/login?userName=wadexu&password=NzrmRcIfIW4=");
+//	}
+//
+//	@Test
+//	public void testGetANonExistUser() {
+//		// given().param("q",
+//		// "java8").when().get("/search").then().body("count", equalTo(2));
+//	}
+}
